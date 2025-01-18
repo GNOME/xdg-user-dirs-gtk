4f5de04d4104e9f78550ded6efcdc9384e714550
Jordan Petridis <jpetridis@gnome.org>
2025-01-18 12:47:30 +0200

    meson: Install user-dirs-update-gtk.desktop in the correct datadir

--- meson_custom_install_desktop_file.sh
+++ meson_custom_install_desktop_file.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-mkdir -p "${MESON_INSTALL_DESTDIR_PREFIX}/data/applications/"
+mkdir -p "${MESON_INSTALL_DESTDIR_PREFIX}/share/applications/"
 cp "${DESTDIR}/etc/xdg/autostart/user-dirs-update-gtk.desktop" \
-   "${MESON_INSTALL_DESTDIR_PREFIX}/data/applications/user-dirs-update-gtk.desktop"
+   "${MESON_INSTALL_DESTDIR_PREFIX}/share/applications/user-dirs-update-gtk.desktop"
 
