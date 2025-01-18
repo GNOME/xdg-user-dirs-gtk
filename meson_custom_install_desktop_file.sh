124da83409531344b34c8a98b3eb4cad6d11e154
Jordan Petridis <jpetridis@gnome.org>
2025-01-18 12:48:02 +0200

    meson: Do not fail if destdir isn't set

--- meson_custom_install_desktop_file.sh
+++ meson_custom_install_desktop_file.sh
@@ -3,6 +3,6 @@
 set -eu
 
 mkdir -p "${MESON_INSTALL_DESTDIR_PREFIX}/share/applications/"
-cp "${DESTDIR}/etc/xdg/autostart/user-dirs-update-gtk.desktop" \
+cp "${DESTDIR:-}/etc/xdg/autostart/user-dirs-update-gtk.desktop" \
    "${MESON_INSTALL_DESTDIR_PREFIX}/share/applications/user-dirs-update-gtk.desktop"
 
